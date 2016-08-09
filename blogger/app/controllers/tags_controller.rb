8d3b75590ca90c936c5925ba3f0916a9b016b5ce
Trevor Nagy <tennagy@hotmail.com>
2016-08-08 22:42:23 -0400

    Basic authentication with Sorcery completed

--- blogger/app/controllers/tags_controller.rb
+++ blogger/app/controllers/tags_controller.rb
@@ -8,7 +8,7 @@ class TagsController < ApplicationController
 	def index
 		@tags = Tag.all
 	end
-
+	before_filter :require_login, only: [:destroy]
 	def destroy
 		# Already declaring in show() method, so just pass that value through
 		@tag = Tag.find(params[:id])
